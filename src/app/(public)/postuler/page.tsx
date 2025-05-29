f8c080615820fc679718e7cb8aa3999209247baf
livio <livio.personne@gmail.com>
2025-05-29 23:52:08 +0200

    feat(postuler): page where all currently recruiting studies can be found

---
+++ src/app/(public)/postuler/page.tsx
@@ -0,0 +1,8 @@
+import { ErrorPage } from '@/components/error';
+
+// TODO: This page contains a view of all studies that are currently recruiting
+// For each study, there will be a short description and button to candidate
+
+export default async function Postuler() {
+    return ErrorPage({ title: "La page n'a pas encore été créée." });
+}
