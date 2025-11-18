c8151d58af81300aeab1324103e50256db22757d
livio <livio.personne@gmail.com>
2025-11-18 19:25:06 +0100

    feat(dev-mode): created base button

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -1,8 +1,24 @@
 import { ViewerProvider } from '@/components/hooks/use-viewer';
 import { getViewer } from '@/data/user';
 
+import { DevModeMenu } from './dev-mode-menu';
+
 export default async function Layout({ children }: { children: React.ReactNode }) {
     const viewerResult = await getViewer();
 
-    return <ViewerProvider value={viewerResult}>{children}</ViewerProvider>;
+    if (viewerResult.status === 'success') {
+    }
+
+    return (
+        <ViewerProvider value={viewerResult}>
+            {
+                <div className="w-full h-full">
+                    <div className="absolute top-14 right-4">
+                        <DevModeMenu viewerResult={viewerResult} />
+                    </div>
+                    <div className="w-full h-full">{children}</div>
+                </div>
+            }
+        </ViewerProvider>
+    );
 }
