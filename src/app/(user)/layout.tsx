4ddbcf3c61962f73ab34a629bb5a31049541e9fa
livio <livio.personne@gmail.com>
2025-11-19 10:01:19 +0100

    fix(dev-mode): made dev tools appear on invalid position error

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -1,22 +1,8 @@
-import { DevModeMenu } from '@/components/dev-mode-menu';
 import { ViewerProvider } from '@/components/hooks/use-viewer';
 import { getViewer } from '@/data/user';
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
     const viewerResult = await getViewer();
 
-    return (
-        <ViewerProvider value={viewerResult}>
-            {
-                <div className="w-full h-full">
-                    {process.env.DEV_MODE ? (
-                        <div className="absolute top-14 right-4">
-                            <DevModeMenu viewerResult={viewerResult} />
-                        </div>
-                    ) : null}
-                    <div className="w-full h-full">{children}</div>
-                </div>
-            }
-        </ViewerProvider>
-    );
+    return <ViewerProvider value={viewerResult}>{children}</ViewerProvider>;
 }
